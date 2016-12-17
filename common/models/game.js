2e6b4202112f971d1af81ea57f37e64560c782d0
Guifeng Zheng <gzheng@hotmail.com>
2016-12-17 21:12:18 +0800

    add model renwu

--- common/models/game.js
+++ common/models/game.js
@@ -1,3 +1,15 @@
 module.exports = function(Game) {
-
+// listGames
+  Game.listGames = function(cb) {
+    Game.find({
+      fields: {
+        level: false,
+        score: false
+      }
+    }, cb);
+  };
+  Game.remoteMethod('listGames', {
+    returns: {arg: 'games', type: 'array'},
+    http: {path:'/list-games', verb: 'get'}
+  });
 };
--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -2,22 +2,31 @@
  * Created by guifeng on 2016/12/5.
  */
 module.exports = function(app) {
-  var User = app.models.user;
+  var Player = app.models.player;
   var Role = app.models.Role;
   var RoleMapping = app.models.RoleMapping;
-  var Game = app.models.game;
+  var Renwu = app.models.renwu;
 
-  User.create([
-    {username: 'demo', email: 'demo@miga.com', password: 'demopass'},
+  Renwu.create(
+    [{xing: "诸葛", ming:"亮", zi:"孔明", xingbie:"男", xugou: false}],
+    function(err, renwu) {
+      if (err) throw err;
+
+      console.log('Created renwu:', renwu);
+    }
+  );
+
+  Player.create([
+    {username: 'DemoUser', email: 'demo@miga.com', password: 'demopass'},
     {username: 'Guifeng', email: 'guifeng@miga.com', password: 'demopass'}
-  ], function(err, users) {
+  ], function(err, players) {
     if (err) throw err;
 
-    console.log('Created users:', users);
+    console.log('Created players:', players);
 
     // create game 1 and make demo the owner
-    users[0].games.create({
-      name: 'demogame',
+    players[0].games.create({
+      name: 'Demo Game',
       level: 1,
       score: 1
     }, function(err, game) {
@@ -26,8 +35,8 @@ module.exports = function(app) {
       console.log('Created game:', game);
     });
 
-    //create project 2 and make jane the owner
-    users[1].games.create({
+    //create project 2 and make guifeng the owner
+    players[1].games.create({
       name: 'My Game',
       level: 2,
       score: 200
@@ -44,10 +53,10 @@ module.exports = function(app) {
 
       console.log('Created role:', role);
 
-      //make bob an admin
+      //make guifeng an admin
       role.principals.create({
         principalType: RoleMapping.USER,
-        principalId: users[1].id
+        principalId: players[1].id
       }, function(err, principal) {
         if (err) throw err;
 
